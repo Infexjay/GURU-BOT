1b2cec13b06c5d151d1e21f1b72a007a118b3f23
Infexjay <97139551+Infexjay@users.noreply.github.com>
2024-05-29 01:51:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -71,14 +71,14 @@ global.APIKeys = {
 // Sticker WM
 global.botname = process.env.BOTNAME
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ'
-global.author = 'ᴳᵘʳᵘ ˢᵉⁿˢᵉⁱ'
+global.packname = 'INFEX┃ᴮᴼᵀ'
+global.author = 'INFEXJAY'
 global.menuvid = 'https://i.imgur.com/AjDzGgG.mp4'
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/guru.json\n'
-global.dygp = 'https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp'
+global.dygp = 'https://chat.whatsapp.com/KXu4FYERcDX6CfwenpFWrV'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT'
-global.fgyt = 'https://youtube.com/@Asliguru'
-global.fgpyp = 'https://youtube.com/@Asliguru'
+global.fgyt = 'https://youtube.com/@iaminfex?si=moyn76ujJnsYA741'
+global.fgpyp = 'https://youtube.com/@iaminfex?si=moyn76ujJnsYA741'
 global.fglog = 'https://raw.githubusercontent.com/Guru322/api/Guru/guru.jpg'
 global.thumb = fs.readFileSync('./Assets/Gurulogo.jpg')
 
